61394c03743952eac5ca3e29514e4293bc68156a
hkseo98 <69430175+hkseo98@users.noreply.github.com>
2021-10-15 12:14:48 +0900

    scroll top

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,15 @@
-import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
-import React from "react";
-import react, { useState, useEffect, useRef } from "react";
-import useScrollSpy from "react-use-scrollspy";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
-const Home: NextPage = () => {
-  const [active, setActive] = useState(0);
-  const sectionRefs = [
-    useRef(null),
-    useRef(null),
-    useRef(null),
-    useRef(null),
-    useRef(null),
-    useRef(null),
-    useRef(null),
-    useRef(null),
-    useRef(null),
-    useRef(null),
-  ];
-
-  let activeSection = useScrollSpy({
-    sectionElementRefs: sectionRefs,
-    offsetPx: -500,
-  });
-
-  useEffect(() => {}, [activeSection]);
-
-  const tabs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
+const Home = () => {
   return (
-    <div className={styles.container}>
-      <nav className={styles.navbar}>
-        {tabs.map((tab, index) => (
-          <span
-            key={index}
-            onClick={() => {
-              sectionRefs[index].current.scrollIntoView({
-                block: "start",
-                behavior: "smooth",
-              });
-            }}
-            className={
-              activeSection === index ? styles.menu_active : styles.menu
-            }
-          >
-            {index + 1}
-          </span>
-        ))}
-      </nav>
-
-      <div className={styles.contents}>
-        <div className={styles.content1} ref={sectionRefs[0]}>
-          1
-        </div>
-        <div className={styles.content2} ref={sectionRefs[1]}>
-          2
-        </div>
-        <div className={styles.content3} ref={sectionRefs[2]}>
-          3
-        </div>
-        <div className={styles.content4} ref={sectionRefs[3]}>
-          4
-        </div>
-        <div className={styles.content5} ref={sectionRefs[4]}>
-          5
-        </div>
-        <div className={styles.content6} ref={sectionRefs[5]}>
-          6
-        </div>
-        <div className={styles.content7} ref={sectionRefs[6]}>
-          7
-        </div>
-        <div className={styles.content8} ref={sectionRefs[7]}>
-          8
-        </div>
-        <div className={styles.content9} ref={sectionRefs[8]}>
-          9
-        </div>
-        <div className={styles.content10} ref={sectionRefs[9]}>
-          10
-        </div>
-      </div>
+    <div>
+      <Link href="/scrollSpying">
+        <a className="menu">scrollSpying</a>
+      </Link>
+      <Link href="/scrollTop">
+        <a className="menu">scrollTop</a>
+      </Link>
     </div>
   );
 };
---
+++ pages/scrollSpying.tsx
@@ -0,0 +1,59 @@
+import type { NextPage } from "next";
+import React from "react";
+import react, { useRef } from "react";
+import useScrollSpy from "react-use-scrollspy";
+
+const ScrollSpy = () => {
+  const sectionRefs = [
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+    useRef(null),
+  ];
+
+  let activeSection = useScrollSpy({
+    sectionElementRefs: sectionRefs,
+    offsetPx: -500,
+  });
+
+  const tabs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  return (
+    <div className="container">
+      <nav className="navbar">
+        {tabs.map((tab, index) => (
+          <span
+            key={index}
+            onClick={() => {
+              sectionRefs[index].current.scrollIntoView({
+                block: "start",
+                behavior: "smooth",
+              });
+            }}
+            className={activeSection === index ? "menu_active" : "menu"}
+          >
+            {index + 1}
+          </span>
+        ))}
+      </nav>
+
+      <div className="contents">
+        {tabs.map((tab, index) => {
+          return (
+            <div className={`content${tab}`} ref={sectionRefs[index]}>
+              {index + 1}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ScrollSpy;
